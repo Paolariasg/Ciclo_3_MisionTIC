a4329f1f07f59024c881a4a0951bd210493fc28e
Paola Arias <andrea-arias1999@hotmail.com>
2021-10-22 22:34:26 -0500

    Pagina_Bootstrap_JS

--- Bootstrap_project/functions.js
+++ Bootstrap_project/functions.js
@@ -1,10 +1,16 @@
 //FUNCIÓN PARA MANIPULAR EL DOM
-function cambiarImagen(){
-    document.getElementById("img1").src = "https://image.goat.com/750/attachments/product_template_pictures/images/060/556/759/original/815560_00.png.png";
+function cambiarImagen() {
+    let alt = document.getElementById("img1").alt;
+    if (alt == "img1") {
+        document.getElementById("img1").src =
+            "https://image.goat.com/750/attachments/product_template_pictures/images/060/556/759/original/815560_00.png.png";
+        document.getElementById("img1").alt = "img2";
+    } else {
+        document.getElementById("img1").src =
+            "https://image.goat.com/750/attachments/product_template_pictures/images/060/172/882/original/CZ4149_400.png.png";
+        document.getElementById("img1").alt = "img1";
+    }
 }
-
-
-
 
 /********* VARIABLES *********/
 const numero_5 = 5;
@@ -13,37 +19,37 @@ let numero_decimal = 10.2;
 
 /********* FUNCIONES *********/
 function saludar() {
-    alert("Hola mundo")
+    alert("Hola mundo");
 }
 
 function operacion() {
-    sumar(5, 2)
+    sumar(5, 2);
 }
 
 function sumar(num1, num2) {
     return num1 + num2;
 }
 
-let functionFlecha = () =>  {
-    console.log("Esto es una función flecha")
-}
+let functionFlecha = () => {
+    console.log("Esto es una función flecha");
+};
 
 /********* CICLOS *********/
 for (let i = 0; i < 10; i++) {
     console.log(i);
 }
 
-let personas = ["Juan", "Ana", "Andrés", "Rubén"]
+let personas = ["Juan", "Ana", "Andrés", "Rubén"];
 for (let i = 0; i < personas.length; i++) {
     console.log(personas[i]);
 }
 
-console.log("-------FORECH-------")
-personas.forEach(element => {
+console.log("-------FORECH-------");
+personas.forEach((element) => {
     console.log(element);
 });
 
-console.log("-------WHILE-------")
+console.log("-------WHILE-------");
 let contador = 0;
 while (contador < personas.length) {
     console.log(personas[contador]);
